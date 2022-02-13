646bb6663dc9ece2bf2ae1febf2e6b280a6c8362
quaverous2 <antonanco9@gmail.com>
2022-02-13 18:30:40 +0100

    Implemented twitter-api-v2 library

--- src/app/services/searchbar.service.ts
+++ src/app/services/searchbar.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+import { TwitterApi } from 'twitter-api-v2';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchbarService {
 
+  client = new TwitterApi('APIKEY');;
+
   constructor() { }
 
-  getUserData() {
-    console.log("API Request")
+  async getUserData() {
+    const homeTimeline = await this.client.v1.homeTimeline();
+
   }
 }
